e62a5bdb8ae2a2f39c0630c0db0da121cd8f2b7a
Eugis <eugenijus.sersniovas@gmail.com>
2018-04-23 22:50:40 +0300

    Implement "createAuthor" functionality

--- src/main/java/lt/vu/usecases/Authors.java
+++ src/main/java/lt/vu/usecases/Authors.java
@@ -6,6 +6,7 @@ import lt.vu.persistence.AuthorsDAO;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Model
@@ -13,18 +14,32 @@ public class Authors {
     @Inject
     private AuthorsDAO authorsDAO;
     private List<Author> allAuthors;
+    private Author newAuthor = new Author();
 
     @PostConstruct
     public void init() {
         this.loadAuthors();
     }
 
+    @Transactional
+    public String createNewAuthor() {
+        authorsDAO.save(newAuthor);
+        return "success";
+    }
+
     private void loadAuthors() {
         this.allAuthors = authorsDAO.loadAll();
     }
 
-
     public List<Author> getAllAuthors() {
         return allAuthors;
+    }
+
+    public Author getNewAuthor() {
+        return newAuthor;
+    }
+
+    public void setNewAuthor(Author newAuthor) {
+        this.newAuthor = newAuthor;
     }
 }
